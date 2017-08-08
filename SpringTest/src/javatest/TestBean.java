7ac7c4181e4043f7709d9961e285510a9b3dba15
fendou666 <ls_code@126.com>
2017-08-08 18:32:57 +0800

    spring测试

---
+++ SpringTest/src/javatest/TestBean.java
@@ -0,0 +1,27 @@
+
+package javatest;
+
+public class TestBean {
+	private int i1;
+	private boolean b1;
+	private String str1;
+	public int getI1() {
+		return i1;
+	}
+	public void setI1(int i1) {
+		this.i1 = i1;
+	}
+	public boolean isB1() {
+		return b1;
+	}
+	public void setB1(boolean b1) {
+		this.b1 = b1;
+	}
+	public String getStr1() {
+		return str1;
+	}
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+	
+}
